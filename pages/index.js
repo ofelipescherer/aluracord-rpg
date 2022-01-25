1dd2d609016777e97df2d9e3b21326b31a28d452
Felipe Scherer <felipescherer25@gmail.com>
2022-01-25 20:43:10 -0300

    feat: state hook and next router

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import { useRouter } from 'next/router';
+import React from 'react';
 import appConfig from '../config.json';
-
-function GlobalStyle() {
-	return (
-		<style global jsx>{`
-			* {
-				margin: 0;
-				padding: 0;
-				box-sizing: border-box;
-				list-style: none;
-			}
-			body {
-				font-family: 'Open Sans', sans-serif;
-			}
-			/* App fit Height */
-			html,
-			body,
-			#__next {
-				min-height: 100vh;
-				display: flex;
-				flex: 1;
-			}
-			#__next {
-				flex: 1;
-			}
-			#__next > * {
-				flex: 1;
-			}
-			/* ./App fit Height */
-		`}</style>
-	);
-}
 
 function Titulo(props) {
 	const Tag = props.tag || 'h1';
@@ -49,11 +20,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-	const username = 'ofelipescherer';
+	const [username, setUsername] = React.useState('ofelipescherer');
+	const router = useRouter();
 
 	return (
 		<>
-			<GlobalStyle />
 			<Box
 				styleSheet={{
 					display: 'flex',
@@ -88,6 +59,10 @@ export default function PaginaInicial() {
 					{/* Formulário */}
 					<Box
 						as="form"
+						onSubmit={(event) => {
+							event.preventDefault();
+							router.push('/chat');
+						}}
 						styleSheet={{
 							display: 'flex',
 							flexDirection: 'column',
@@ -110,6 +85,8 @@ export default function PaginaInicial() {
 						</Text>
 
 						<TextField
+							value={username}
+							onChange={(event) => setUsername(event.target.value)}
 							fullWidth
 							textFieldColors={{
 								neutral: {
@@ -120,6 +97,7 @@ export default function PaginaInicial() {
 								},
 							}}
 						/>
+
 						<Button
 							type="submit"
 							label="Entrar"
